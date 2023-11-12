b5c1a2fdf301caa4184ff2fdaa1112d50c4edd65
Soham Meher <sohammeher98@gmail.com>
2023-11-12 15:35:24 +0530

    Changes in ui

--- frontend/src/pages/Predict.js
+++ frontend/src/pages/Predict.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "./Predict.css";
-import APIService from "./APIService.js";
 import axios from "axios";
 class Predict extends Component {
   constructor(props) {
@@ -8,42 +7,56 @@ class Predict extends Component {
     this.state = {
       salary: 0,
     };
+    this.handleBodyClick = this.handleBodyClick.bind(this);
   }
-  submit = (e) => {
-    e.preventDefault();
-    // APIService.postPredict({
-    //   experience: document.getElementById("experience").value,
-    //   testScore: document.getElementById("testScore").value,
-    //   interviewScore: document.getElementById("interviewScore").value,
-    // })
-    //   .then((response) => console.log("got response"))
-    //   .catch((error) => console.log("error", error));
+
+  handleBodyClick() {
     axios
-      .post("http://localhost:5000/predict", {
-        experience: document.getElementById("experience").value,
-        testScore: document.getElementById("testScore").value,
-        interviewScore: document.getElementById("interviewScore").value,
+      .post("http://sohammeher.pythonanywhere.com/predict", {
+        experience: parseInt(document.getElementById("experience").value),
+        testScore: parseInt(document.getElementById("testScore").value),
+        interviewScore: parseInt(
+          document.getElementById("interviewScore").value
+        ),
       })
       .then((res) => {
         this.setState({ salary: res.data });
-        console.log(res.data);
+        console.log("Response from flask: ", res.data);
       })
       .catch((error) => console.log(error));
-  };
+  }
+
   render() {
     return (
       <div>
         <div className="predict">
           <form onSubmit={this.submit}>
-            <input id="experience" placeholder="Experience" />
-            <input id="testScore" placeholder="Test Score" />
-            <input id="interviewScore" placeholder="Interview Score" />
-            <button type="submit" id="submit-button">
+            <input type="number" id="experience" placeholder="Experience" />
+            <input
+              type="number"
+              min="0"
+              max="10"
+              id="testScore"
+              placeholder="Test Score (out of 10)"
+            />
+            <input
+              type="number"
+              min="0"
+              max="10"
+              id="interviewScore"
+              placeholder="Interview Score (out of 10)"
+            />
+
+            <div onClick={this.handleBodyClick} className="coolButton">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
               Submit
-            </button>
+            </div>
           </form>
         </div>
-        <h1>Predicted salary of using above values is: ${this.state.salary}</h1>
+        <h1>Predicted salary using above values is: ${this.state.salary}</h1>
       </div>
     );
   }
